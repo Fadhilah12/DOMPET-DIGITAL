a9eb4d7cff1721488b070473f69c4829bb7d64ba
unknown <yusa82401@gmail.com>
2023-07-18 08:40:17 +0700

    update fix

--- resources/views/dashbord.blade.php
+++ resources/views/dashbord.blade.php
@@ -39,8 +39,8 @@
                         </a>
                         <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                                <a class="nav-link" href="layout-static.html">Tambah User</a>
+                                <a class="nav-link" href="layout-sidenav-light.html">Show User</a>
                             </nav>
                         </div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
